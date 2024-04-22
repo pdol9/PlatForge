936092592b3064bfb5dfa7fefd12864db61e3343
pdol <111592903+pdol9@users.noreply.github.com>
2024-04-22 17:46:12 +0200

    fix containers -> interactivity

--- srcs/setup.sh
+++ srcs/setup.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-echo "work in progress" >> progress
-cd */ && exec make "$@"
+# colour codes
+CLEAR="\033[0m"
+PR="\033[1;35m"
+
+# Find all directories in the current directory
+num_directories=$(find . -maxdepth 1 -type d | wc -l)
+
+# Check if there's more than one directory
+if [ $num_directories -gt 2 ]; then
+	echo -e "${PR}Error: More than one directory found.\n/srcs/ directory should only contain one project.${CLEAR}"
+	exit 1
+else
+	# initialise application
+	cd */ && make
+	/bin/bash
+fi
